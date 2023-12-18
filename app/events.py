a48f673a0b8017ed1d20a1a96941191d91f5c559
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2023-12-18 16:45:35 -0500

    Creando controladores

---
+++ app/events.py
@@ -0,0 +1,11 @@
+from flask import request
+from flask_socketio import emit
+
+from .extensions import socketio
+##from .models.Menu_Controller import MenuController
+#from .models.Order_Controller import OrderController
+#from .models.Order_Controller import Order
+#from .models.Frequency_Controller import FrequencyController
+##from .controllers import menu_controller
+##from .controllers import order_controller
+##from .controllers import frequency_controller
